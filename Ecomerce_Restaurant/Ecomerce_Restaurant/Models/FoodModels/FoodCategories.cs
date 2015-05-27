68fc2bd6c833a893b2fded599c88b5d11e19d7d9
stpriyanka <stpriyanka2011@gmail.com>
2015-05-28 00:54:25 +0200

    revert on working copy

--- Ecomerce_Restaurant/Ecomerce_Restaurant/Models/FoodModels/FoodCategories.cs
+++ Ecomerce_Restaurant/Ecomerce_Restaurant/Models/FoodModels/FoodCategories.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-using PagedList;
+
 
 namespace Ecomerce_Restaurant.Models.FoodModels
 {
