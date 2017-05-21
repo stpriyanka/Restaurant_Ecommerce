a0b94003bbf7e2806ff361ee2f86f30f0b92f7cf
Priyanka S.Tasnia <stpriyanka2011@gmail.com>
2017-05-21 12:15:20 +0200

    fixed doublbug onh to script

---
+++ Ecomerce_Restaurant/Ecomerce_Restaurant/Scripts/CustomScript.js
@@ -0,0 +1,10 @@
+﻿var chatVisibility = function () {
+	if (document.querySelector('#collapseOne').style.display == 'none') {
+		document.querySelector('#collapseOne').style.display = 'block';
+	}
+
+	else
+	{
+		document.getElementById('collapseOne').style.display = 'none';
+	}
+};
--- Ecomerce_Restaurant/Ecomerce_Restaurant/Scripts/chatScript.js
+++ Ecomerce_Restaurant/Ecomerce_Restaurant/Scripts/chatScript.js
@@ -1,37 +1,50 @@
 ﻿/// <reference path="jquery-3.1.1.min.js" />
 /// <reference path="jquery.signalR-2.2.2.min.js" />
 
+var getName = function () {
+	//var name1 = document.querySelector('#start1').value;
+	document.getElementById('start1').style.visibility = 'hidden';
+	document.getElementById('startButton').style.visibility = 'hidden';
+}
+
 
 $(function () {
+	console.log($.connection);
+	// Declare a proxy to reference the hub.
+	var chat = $.connection.chatHub;
 
-		console.log($.connection);
-		// Declare a proxy to reference the hub.
-		var chat = $.connection.chatHub;
+	console.log('Now connected, connection ID=' + $.connection.hub.id);
 
-		console.log('Now connected, connection ID=' + $.connection.hub.id);
+	// Create a function that the hub can call to broadcast messages.
+	chat.client.broadcastMessage = function (name, message) {
+		// Html encode display name and message.
 
-		// Create a function that the hub can call to broadcast messages.
-		chat.client.broadcastMessage = function (name, message) {
-			// Html encode display name and message.
-			var encodedName = $('<div />').text(name).html();
-			var encodedMsg = $('<div />').text(message).html();
-			// Add the message to the page.
-			$('#discussion').append('<li><strong>' + encodedName
-				+ '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
-			console.log(encodedName + ' ' + encodedMsg);
-
-		};
-		// Get the user name and store it to prepend to messages.
-		$('#displayname').val(prompt('Enter your name:', ''));
-		// Set initial focus to message input box.
-		$('#message').focus();
-		// Start the connection.
-		$.connection.hub.start().done(function () {
-			$('#sendmessage').click(function () {
-				// Call the Send method on the hub.
-				chat.server.send($('#displayname').val(), $('#message').val());
-				// Clear text box and reset focus for next comment.
-				$('#message').val('').focus();
-			});
+		
+		document.getElementById('start1').style.visibility = 'hidden';
+		var encodedName = name;
+		var encodedMsg = $('<div />').text(message).html();
+		
+		// Add the message to the page.
+		$('#discussion').append('<li><strong>' + encodedName
+			+ '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+		return;
+	}
+	// Get the user name and store it to prepend to messages.
+	//$('#displayname').val(prompt('Enter your name:', ''));
+	//$('#displayname').val("Enter name", '');
+	// Set initial focus to message input box.
+	$('#message').focus();
+	// Start the connection.
+	$.connection.hub.start().done(function () {
+		$('#sendmessage').click(function () {
+			var name1 = document.querySelector('#start1').value;
+			if (!name1) {
+				name1 = "Guest";
+			}
+			// Call the Send method on the hub.
+			chat.server.send(name1, $('#message').val());
+			// Clear text box and reset focus for next comment.
+			$('#message').val('').focus();
 		});
 	});
+});
